7eb93bffa10f606d42011a1c37d7e624984cd5dd
S.H. Hayakawa <shuhei@km.phys.sci.osaka-u.ac.jp>
2018-01-10 15:16:52 +0900

    Modify link.sh

--- link.sh
+++ link.sh
@@ -25,7 +25,7 @@ option=
 if [ $1 = "-d" -o $1 = "--dry" ]; then
     option=dry
 elif [ $1 = "-e" -o $1 = "--exec" ]; then
-    option=exec
+    option=exe
 elif [ $1 = "-n" -o $1 = "--new" ]; then
     option=new
 elif [ $1 = "-f" -o $1 = "--force" ]; then
@@ -50,22 +50,27 @@ function makelink()
     src=$1
     dst=$2
 
-    if [ $option = "dry" ]; then
-	echo ln -s $src $dst
-    elif [ $option = "exec" ]; then
-	real1=$($readlink -f $src)
-	real2=$($readlink -f $dst)
-	if [ $real1 = $real2 ]; then
-	    echo $dst is already linking
-	else
-	    ls -l $src
-	    ln -isv $src $dst
-	fi
-    elif [ $option = "new" ]; then
-	[ -e $dst -o -L $dst ] || ln -sv $src $dst
-    elif [ $option = "force" ]; then
-	ln -fsv $src $dst
-    fi
+    case $option in
+	dry)
+	    echo ln -s $src $dst
+	    ;;
+	exe)
+	    real1=$($readlink -f $src)
+	    real2=$($readlink -f $dst)
+	    if [ $real1 = $real2 ]; then
+		echo $dst is already linking
+	    else
+		ls -l $src
+		ln -isv $src $dst
+	    fi
+	    ;;
+	new)
+	    [ -e $dst -o -L $dst ] || ln -sv $src $dst
+	    ;;
+	force)
+	    ln -fsv $src $dst
+	    ;;
+    esac
 }
 
 #_______________________________________________________________________________
