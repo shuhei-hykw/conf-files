2237c0e5b6800d0cacb043b5dfa47dfeea41020f
S.H. Hayakawa <shuhei@km.phys.sci.osaka-u.ac.jp>
2018-10-29 17:10:35 +0900

    update for kekcc

--- root/rootlogon.C
+++ root/rootlogon.C
@@ -8,39 +8,45 @@
   gStyle->SetStatY(0.9);
   gStyle->SetStatH(0.2);
   gStyle->SetStatW(0.2);
-  gStyle->SetPadGridX(true);
-  gStyle->SetPadGridY(true);
+  // gStyle->SetPadGridX(kTRUE);
+  // gStyle->SetPadGridY(kTRUE);
+  // gStyle->SetPadTickX(kTRUE);
+  // gStyle->SetPadTickY(kTRUE);
 
   {//////////////////// 1x1
     TStyle *myStyle = new TStyle("MyStyle","MyStyle");
     gROOT->GetStyle("Modern")->Copy(*myStyle);
     //gROOT->GetStyle("Plain")->Copy(*myStyle);
+    myStyle->SetOptStat(0);
+    myStyle->SetPadGridX(0);
+    myStyle->SetPadGridY(0);
 
     // const Int_t myfont = 42;
-    const Int_t myfont = 22;
-    //const Int_t myfont = 132;
+    //const Int_t myfont = 22;
+    const Int_t myfont = 132;
 
     // Frame margin
-    /* myStyle->SetPadBottomMargin(0.12); */
-    /* myStyle->SetPadLeftMargin(0.12); */
-    /* myStyle->SetPadTopMargin(0.05); */
-    /* myStyle->SetPadBorderSize(0); */
+    myStyle->SetPadBottomMargin(0.12);
+    myStyle->SetPadLeftMargin(0.18);
+    //myStyle->SetPadRightMargin(0.05);
+    myStyle->SetPadTopMargin(0.05);
+    myStyle->SetPadBorderSize(0);
 
     // Grid
-    myStyle->SetPadGridX(true);
-    myStyle->SetPadGridY(true);
+    myStyle->SetPadGridX(kTRUE);
+    myStyle->SetPadGridY(kTRUE);
 
     // Tick
-    myStyle->SetPadTickX(true);
-    myStyle->SetPadTickY(true);
+    myStyle->SetPadTickX(kTRUE);
+    myStyle->SetPadTickY(kTRUE);
 
     // Font Label Title
     myStyle->SetLabelFont(myfont,"xyz");
     myStyle->SetTitleFont(myfont,"xyz");
 
     // Size Label Title
-    /* myStyle->SetLabelSize(0.045,"xyz"); */
-    /* myStyle->SetTitleSize(0.055,"xyz"); */
+    myStyle->SetLabelSize(0.045,"xyz");
+    myStyle->SetTitleSize(0.055,"xyz");
 
     // Align
     // myStyle->SetTitleAlign(22);
@@ -86,5 +92,5 @@
     /* TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont); */
     gStyle->SetNumberContours(NCont);
   }
-
+  gROOT->SetStyle("MyStyle");
 }
