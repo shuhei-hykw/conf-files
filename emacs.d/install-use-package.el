084402fccca70c9fedeb4b1f2d4baec310ac84f8
Shuhei Hayakawa <hayashu@post.j-parc.jp>
2019-06-14 16:33:26 +0900

    Add emacs.d/install-use-package.el

---
+++ emacs.d/install-use-package.el
@@ -0,0 +1,12 @@
+;;
+;;  Run:
+;;    $ emacs --debug-init --script install-use-package.el
+;;
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+(package-refresh-contents)
+(package-install 'use-package)
