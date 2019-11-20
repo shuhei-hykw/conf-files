0822852f541e6c65f9a7c5a26a22a74dbc7fdace
Shuhei Hayakawa <shuhei@shmba.local>
2019-11-20 22:43:59 +0900

    Add macos/brew_update.sh and GNU utils for macos

---
+++ macos/brew_update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#set -e
+
+#_______________________________________________________________________________
+cmd=$(basename $0)
+brew=brew
+time_stamp=$(date +%Y%m%d)
+#log_dir=/tmp
+#log_dir=$HOME/tmp
+log_dir=$HOME
+#log_file=$log_dir/brew_update.$time_stamp
+log_file=$log_dir/brew_update.log
+
+#_______________________________________________________________________________
+function dobrew()
+{
+    echo | tee -a $log_file
+    echo \$ $brew $1 $2 | tee -a $log_file
+    $brew $1 $2 2>&1 | tee -a $log_file
+}
+
+date | tee $log_file
+dobrew update
+dobrew upgrade
+dobrew cleanup
+dobrew list --versions
+dobrew doctor
